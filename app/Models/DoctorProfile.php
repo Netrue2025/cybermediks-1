2598b0d01836472853fd6bdd8aa74791237f0605
coder-frank <frankemmanuel249@gmail.com>
2025-11-07 11:48:10 +0100

    fixed

--- app/Models/DoctorProfile.php
+++ app/Models/DoctorProfile.php
@@ -8,7 +8,7 @@ class DoctorProfile extends Model
 {
     protected $fillable = ['doctor_id', 'title', 'bio', 'is_available', 'consult_fee', 'avg_duration', 'meeting_link', 'link_updated_at', 'lat', 'lng'];
 
-    protected $casts = ['is_available' => 'bool', 'consult_fee'  => \App\Casts\DisplayMoney::class . ':NGN', 'avg_duration' => 'int', 'lat' => 'decimal:7', 'lng' => 'decimal:7', 'link_updated_at' => 'datetime'];
+    protected $casts = ['is_available' => 'bool', 'consult_fee' => 'decimal:2', 'avg_duration' => 'int', 'lat' => 'decimal:7', 'lng' => 'decimal:7', 'link_updated_at' => 'datetime'];
     public function doctor()
     {
         return $this->belongsTo(User::class, 'doctor_id');
